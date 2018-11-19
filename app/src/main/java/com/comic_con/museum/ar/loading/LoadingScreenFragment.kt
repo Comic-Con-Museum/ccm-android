8357435db043af1bbab50efb8feceed20f8d7adb
Christians, Ben <bbc7909@rit.edu>
2018-11-18 17:24:57 -0800

    Added GIF rendering to the loading screen

--- app/src/main/java/com/comic_con/museum/ar/loading/LoadingScreenFragment.kt
+++ app/src/main/java/com/comic_con/museum/ar/loading/LoadingScreenFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.comic_con.museum.ar.CCMApplication
 import com.comic_con.museum.ar.MainActivity
 import com.comic_con.museum.ar.R
@@ -26,6 +28,14 @@ class LoadingScreenFragment: Fragment() {
         val thisView = inflater.inflate(R.layout.fragment_loading_screen, container, false)
 
         loadingScreenViewModel.initLoading().observeForever(this::completeLoading)
+
+        val loadingImageView = thisView?.findViewById<ImageView>(R.id.loading_image) ?: return thisView
+
+        context?.let { nonNullContext ->
+            Glide.with(nonNullContext)
+                .load(R.raw.loading)
+                .into(loadingImageView)
+        }
 
         return thisView
     }
