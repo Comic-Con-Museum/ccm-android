6bab64b009947a061e91fc6948d44e4e80468358
Christians, Ben <bbc7909@rit.edu>
2018-11-26 14:09:55 -0800

    Added documentation

--- app/src/main/java/com/comic_con/museum/ar/experience/ExperienceActivity.kt
+++ app/src/main/java/com/comic_con/museum/ar/experience/ExperienceActivity.kt
@@ -3,7 +3,6 @@ package com.comic_con.museum.ar.experience
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
@@ -11,12 +10,12 @@ import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.view.MenuItem
-import android.view.WindowManager
 import com.comic_con.museum.ar.CCMApplication
 import com.comic_con.museum.ar.R
 import com.comic_con.museum.ar.experience.nav.BottomNavListener
 import com.comic_con.museum.ar.experience.nav.BottomNavOnPageChangeListener
 import com.comic_con.museum.ar.experience.progress.ProgressViewModel
+import com.comic_con.museum.ar.overview.ExhibitModel
 import com.comic_con.museum.ar.overview.OverviewViewModel
 import com.comic_con.museum.ar.views.ExhibitCard
 import com.unity3d.player.UnityPlayer
@@ -37,7 +36,7 @@ class ExperienceActivity: AppCompatActivity() {
     @Inject
     lateinit var progressViewModel: ProgressViewModel
 
-    private var experienceId: String? = null
+    private var experienceModel: ExhibitModel? = null
 
     // The unity player for the AR component
     val unityPlayer: UnityPlayer by lazy {
@@ -51,11 +50,11 @@ class ExperienceActivity: AppCompatActivity() {
 
         // Get the experience model associated with the selected experience
         val experienceId = intent?.extras?.getString(ExhibitCard.EXPERIENCE_ID_KEY) ?: throw IllegalStateException("Experience was started with null experienceId")
-        this.experienceId = experienceId
         val experienceModel = overviewViewModel.exhibitModelsLiveData.value?.filter { model ->
             model.exhibitId == experienceId
         }?.getOrNull(0) ?: throw IllegalStateException("Experience was started with no valid experience model. ExperienceId: $experienceId")
         experienceViewModel.setExperience(experienceModel)
+        this.experienceModel = experienceModel
 
         // Set up the toolbar
         toolbar = supportActionBar
@@ -82,15 +81,37 @@ class ExperienceActivity: AppCompatActivity() {
     }
 
     /**
-     * Called from Unity when a new item is collected
+     * Called (from Unity) when a new item is collected
+     * @param contentId: The contentId of the content item collected
+     * @return Int correlating to how the collection event was handled
+     *  *Success Codes*
+     *  0:   New content item was collected, and associated progress items were updated
+     *  1:   New content item was collected, but not associated with a progress item
+     *  2:   Content item was already collected
+     *  *Error Codes*
+     *  500: Experience was null in the activity, so progress could not be added to an associated experience
+     *  501: Experience does not contain the given Content ID
      */
     @Suppress("unused")
     fun newCollectionEvent(contentId: String): Int {
-        this.experienceId?.let { experienceId ->
+        this.experienceModel?.exhibitId?.let { experienceId ->
+            // If the contentId is not associated with the experience
+            if( experienceModel?.content?.contentItems?.asSequence()?.map{ it.contentId }?.contains(contentId) != true ) {
+                return 501
+            }
+            // If the content item was already collected
+            if( experienceModel?.progress?.achievedContentItems?.contains(contentId) == true ) {
+                return 2
+            }
+            // If the contentId is not found as an item associated with a progress
+            if( experienceModel?.progress?.progressItems?.flatMap{ it.contentItems }?.contains(contentId) != true ) {
+                return 1
+            }
+            // If uncollected new item
             progressViewModel.updateProgress(experienceId, contentId)
             return 0
         }
-        return 1
+        return 500
     }
 
     private fun switchToFragment(fragment: Fragment, tag: String?) {
