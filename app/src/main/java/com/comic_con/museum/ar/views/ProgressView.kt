bf691060164d3cf570bfcbdce7099b2de064d19a
Christians, Ben <bbc7909@rit.edu>
2018-11-27 15:50:14 -0800

    Fixed progress fill logic.

--- app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
+++ app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
@@ -2,6 +2,7 @@ package com.comic_con.museum.ar.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import com.comic_con.museum.ar.R
@@ -25,9 +26,8 @@ class ProgressView(c: Context, a: AttributeSet): LinearLayout(c, a) {
         this.findViewById<TextView>(R.id.achievedProgressNum)?.text = relevantAchievedItems.size.toString()
         this.findViewById<TextView>(R.id.maxProgressNum)?.text = progress.contentItems.size.toString()
 
-        val progressFill = this.findViewById<ImageView>(R.id.progressBar)
-        val parentWidth = (progressFill.parent as? View)?.layoutParams?.width ?: 0
-        val fillPercent = (relevantAchievedItems.size.toFloat() / progress.contentItems.size.toFloat())
-        progressFill?.layoutParams?.width = (parentWidth * fillPercent).toInt()
+        // Set progress bar
+        this.findViewById<LinearLayout>(R.id.progressBarContainer)?.weightSum = progress.contentItems.size.toFloat()
+        (this.findViewById<View>(R.id.progressBar)?.layoutParams as? LinearLayout.LayoutParams)?.weight = relevantAchievedItems.size.toFloat()
     }
 }
