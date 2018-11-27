07a85578d04a64e5392496f52e58c615ea9915a7
Christians, Ben <bbc7909@rit.edu>
2018-11-27 15:05:03 -0800

    Initial implementation of the new layout

--- app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
+++ app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
@@ -3,34 +3,34 @@ package com.comic_con.museum.ar.views
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
-import android.widget.RelativeLayout
 import android.widget.TextView
 import com.comic_con.museum.ar.R
 import com.comic_con.museum.ar.experience.progress.Progress
-import android.util.TypedValue
+import android.view.View
+import android.widget.LinearLayout
 import com.comic_con.museum.ar.experience.progress.ProgressModel
 
 
-class ProgressView(c: Context, a: AttributeSet): RelativeLayout(c, a) {
+class ProgressView(c: Context, a: AttributeSet): LinearLayout(c, a) {
 
     fun setProgress(progressModel: ProgressModel, progress: Progress?) {
-        this.findViewById<TextView>(R.id.progress_text).text = progress?.progressName
+        this.findViewById<TextView>(R.id.progressTitle).text = progress?.progressName
 
         val relevantAchievedItems = progressModel.achievedContentItems.filter { achievedContentId ->
             achievedContentId in (progress?.contentItems ?: emptyList())
         }
 
-        val progressFill = this.findViewById<ImageView>(R.id.progress_fill)
-        progressFill?.layoutParams?.height = dpToPx(
-            100 * (relevantAchievedItems.size.toFloat()/(progress?.contentItems?.size?.toFloat() ?: Float.MAX_VALUE))
-        )
+        val progressFill = this.findViewById<ImageView>(R.id.progressBar)
+        val parentWidth = (progressFill.parent as? View)?.layoutParams?.width ?: 0
+        val fillPercent = (relevantAchievedItems.size.toFloat()/(progress?.contentItems?.size?.toFloat() ?: Float.MAX_VALUE))
+        progressFill?.layoutParams?.width = (parentWidth * fillPercent).toInt()
     }
 
-    private fun dpToPx(dp: Float): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            dp,
-            resources.displayMetrics
-        ).toInt()
-    }
+//    private fun dpToPx(dp: Float): Int {
+//        return TypedValue.applyDimension(
+//            TypedValue.COMPLEX_UNIT_DIP,
+//            dp,
+//            resources.displayMetrics
+//        ).toInt()
+//    }
 }
