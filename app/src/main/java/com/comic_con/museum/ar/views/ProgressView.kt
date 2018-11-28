7af3acd6aa2ef34d2b7c56c97b6e7cb3d428ac9e
Christians, Ben <bbc7909@rit.edu>
2018-11-27 16:09:50 -0800

    Added completed progress visuals.

--- app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
+++ app/src/main/java/com/comic_con/museum/ar/views/ProgressView.kt
@@ -1,18 +1,31 @@
 package com.comic_con.museum.ar.views
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
-import android.util.Log
-import android.widget.ImageView
 import android.widget.TextView
 import com.comic_con.museum.ar.R
 import com.comic_con.museum.ar.experience.progress.Progress
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.comic_con.museum.ar.experience.progress.ProgressModel
 
 
 class ProgressView(c: Context, a: AttributeSet): LinearLayout(c, a) {
+
+    private val titleText by lazy {
+        this.findViewById<TextView>(R.id.progressTitle)
+    }
+    private val achievedProgressText by lazy {
+        this.findViewById<TextView>(R.id.achievedProgressNum)
+    }
+    private val maxProgressNum by lazy {
+        this.findViewById<TextView>(R.id.maxProgressNum)
+    }
+    private val progressBarContainer by lazy {
+        this.findViewById<LinearLayout>(R.id.progressBarContainer)
+    }
 
     fun setProgress(progressModel: ProgressModel, progress: Progress?) {
         progress ?: return
@@ -22,12 +35,34 @@ class ProgressView(c: Context, a: AttributeSet): LinearLayout(c, a) {
         }
 
         // Set text values
-        this.findViewById<TextView>(R.id.progressTitle)?.text = progress.progressName
-        this.findViewById<TextView>(R.id.achievedProgressNum)?.text = relevantAchievedItems.size.toString()
-        this.findViewById<TextView>(R.id.maxProgressNum)?.text = progress.contentItems.size.toString()
+        titleText?.text = progress.progressName
 
-        // Set progress bar
-        this.findViewById<LinearLayout>(R.id.progressBarContainer)?.weightSum = progress.contentItems.size.toFloat()
-        (this.findViewById<View>(R.id.progressBar)?.layoutParams as? LinearLayout.LayoutParams)?.weight = relevantAchievedItems.size.toFloat()
+        // If progress complete
+        if (relevantAchievedItems.size >= progress.contentItems.size) {
+            // Update background
+            this.background = ContextCompat.getDrawable(this.context, R.drawable.progress_background_complete)
+
+            // Update progress bar
+            progressBarContainer.background = ContextCompat.getDrawable(this.context, R.drawable.progress_bar_complete)
+
+            // Update text color
+            titleText.setTextColor(ContextCompat.getColor(this.context, R.color.black))
+
+            // Hide/update progress numbers
+            maxProgressNum?.visibility = View.INVISIBLE
+            achievedProgressText?.text = this.context.getString(R.string.progress_completed)
+            achievedProgressText?.setTextColor(ContextCompat.getColor(this.context, R.color.black))
+        }
+        // Else progress is not complete
+        else {
+            achievedProgressText?.text = relevantAchievedItems.size.toString()
+            maxProgressNum?.text = progress.contentItems.size.toString()
+
+            // Set progress bar
+            progressBarContainer?.weightSum = progress.contentItems.size.toFloat()
+            (this.findViewById<View>(R.id.progressBar)?.layoutParams as? LinearLayout.LayoutParams)?.weight =
+                    relevantAchievedItems.size.toFloat()
+
+        }
     }
 }
